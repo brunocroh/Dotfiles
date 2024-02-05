f2a99db18ec14013a9d1647f71b065016b5d15aa
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-05 13:50:21 -0300

    update sketchybar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff181926
+export WHITE=0xffcad3f5
+export RED=0xffed8796
+export GREEN=0xffa6da95
+export BLUE=0xff8aadf4
+export YELLOW=0xfffbaf02
+export ORANGE=0xfff5a97f
+export MAGENTA=0xffc6a0f6
+export GREY=0xff939ab7
+export PASTEL=0xffebdbb2
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xff282828
+export BAR_BORDER_COLOR=0xfffbaf02 #0xa024273a
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0xcf262626
+export BACKGROUND_2=0xff3a3a3a
+
+export POPUP_BACKGROUND_COLOR=0xff1e1e2e
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
