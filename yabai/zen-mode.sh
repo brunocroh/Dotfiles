807c5b77c79c574b6e0ffbdee8758709c9c8abbb
Bruno Rodrigues <brunocroh@gmail.com>
2025-03-02 08:50:37 +0000

    update(yabai): add script to listend events and enable/disable zen mode

---
+++ yabai/zen-mode.sh
@@ -0,0 +1,12 @@
+windows=$(yabai -m query --windows --display 1 | jq '[.[] | select(."is-visible"==true and ."is-floating"==false)] | length')
+space=$(yabai -m query --windows --display 1 | jq '[.[] | select(."is-visible"==true and ."is-floating"==false)][0].space')
+
+if [[ $windows == 1 ]]; then
+  yabai -m config --space $space left_padding 600
+  yabai -m config --space $space right_padding 600
+  yabai -m space --space $space --balance
+else
+  yabai -m config --space $space left_padding 4
+  yabai -m config --space $space right_padding 4
+  yabai -m space --space $space --balance
+fi
