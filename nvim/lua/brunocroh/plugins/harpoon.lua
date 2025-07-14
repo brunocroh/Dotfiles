fc7f1d1694552f69e2558974273013b2b100d6b6
Bruno Rodrigues <brunocroh@gmail.com>
2025-07-14 21:09:35 +0100

    update(nvim): update harpoon config

--- nvim/lua/brunocroh/plugins/harpoon.lua
+++ nvim/lua/brunocroh/plugins/harpoon.lua
@@ -3,6 +3,7 @@ local harpoon = require('harpoon')
 harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
