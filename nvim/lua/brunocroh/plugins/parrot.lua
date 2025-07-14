a802411812281d82f967194e0136ac27bd6883e6
Bruno Rodrigues <brunocroh@gmail.com>
2025-07-14 21:09:19 +0100

    update(nvim): update parrot config

--- nvim/lua/brunocroh/plugins/parrot.lua
+++ nvim/lua/brunocroh/plugins/parrot.lua
@@ -1,8 +1,45 @@
 require("parrot").setup {
+  endpoint = '',
   -- Providers must be explicitly added to make them available.
   providers = {
     anthropic = {
+      name = "anthropic",
+      endpoint = "https://api.anthropic.com/v1/messages",
+      model_endpoint = "https://api.anthropic.com/v1/models",
       api_key = os.getenv "ANTHROPIC_API_KEY",
+      params = {
+        chat = { max_tokens = 4096 },
+        command = { max_tokens = 4096 },
+      },
+      topic = {
+        model = "claude-3-5-haiku-latest",
+        params = { max_tokens = 32 },
+      },
+      headers = function(self)
+        return {
+          ["Content-Type"] = "application/json",
+          ["x-api-key"] = self.api_key,
+          ["anthropic-version"] = "2023-06-01",
+        }
+      end,
+      models = {
+        "claude-sonnet-4-20250514",
+        "claude-3-7-sonnet-20250219",
+        "claude-3-5-sonnet-20241022",
+        "claude-3-5-haiku-20241022",
+      },
+      preprocess_payload = function(payload)
+        for _, message in ipairs(payload.messages) do
+          message.content = message.content:gsub("^%s*(.-)%s*$", "%1")
+        end
+        if payload.messages[1] and payload.messages[1].role == "system" then
+          -- remove the first message that serves as the system prompt as anthropic
+          -- expects the system prompt to be part of the API call body and not the messages
+          payload.system = payload.messages[1].content
+          table.remove(payload.messages, 1)
+        end
+        return payload
+      end,
     },
     -- gemini = {
     --   api_key = os.getenv "GEMINI_API_KEY",
@@ -28,9 +65,9 @@ require("parrot").setup {
     -- xai = {
     --   api_key = os.getenv "XAI_API_KEY",
     -- },
-    deepseek = {
-      api_key = os.getenv "DEEPSEEK_API_KEY",
-    },
+    -- deepseek = {
+    --   api_key = os.getenv "DEEPSEEK_API_KEY",
+    -- },
   },
   cmd_prefix = "Ia",
   hooks = {
