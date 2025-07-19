02a3f69196794f9798a82b24d122f7a968369095
Bruno Rodrigues <brunocroh@gmail.com>
2025-07-19 18:01:16 +0100

    update(yabai): bring back yabai balance function call

--- yabai/zen-mode.sh
+++ yabai/zen-mode.sh
@@ -4,9 +4,9 @@ space=$(yabai -m query --windows --display 1 | jq '[.[] | select(."is-visible"==
 if [[ $windows == 1 ]]; then
   yabai -m config --space $space left_padding 600
   yabai -m config --space $space right_padding 600
-  # yabai -m space --space $space --balance
+  yabai -m space --space $space --balance
 else
   yabai -m config --space $space left_padding 12
   yabai -m config --space $space right_padding 12
-  # yabai -m space --space $space --balance
-f
+  yabai -m space --space $space --balance
+fi
