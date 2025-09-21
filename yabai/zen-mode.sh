12fac35db51dc7ad4d37a1615a39c41e5571a596
Bruno Rodrigues <swe@brunocroh.com>
2025-09-21 16:49:14 +0100

    update(yabai): persist if space is zen-mod to prevent unecessary screen recalculations

--- yabai/zen-mode.sh
+++ yabai/zen-mode.sh
@@ -1,12 +1,25 @@
 windows=$(yabai -m query --windows --display 1 | jq '[.[] | select(."is-visible"==true and ."is-floating"==false)] | length')
 space=$(yabai -m query --windows --display 1 | jq '[.[] | select(."is-visible"==true and ."is-floating"==false)][0].space')
 
-if [[ $windows == 1 ]]; then
-  yabai -m config --space $space left_padding 600
-  yabai -m config --space $space right_padding 600
-  yabai -m space --space $space --balance
+if [[ $windows -eq 1 ]]; then
+  isZen=true
 else
-  yabai -m config --space $space left_padding 12
-  yabai -m config --space $space right_padding 12
-  yabai -m space --space $space --balance
+  isZen=false
+fi
+
+zen_state_file="$HOME/.config/yabai/.zen_state_space_$space"
+previous_zen_state=$(cat "$zen_state_file" 2>/dev/null || echo "false")
+
+if [[ $isZen != $previous_zen_state ]]; then
+  echo "$isZen" > "$zen_state_file"
+  
+  if [[ $isZen == true ]]; then
+    yabai -m config --space $space left_padding 600
+    yabai -m config --space $space right_padding 600
+    yabai -m space --space $space --balance
+  else
+    yabai -m config --space $space left_padding 12
+    yabai -m config --space $space right_padding 12
+    yabai -m space --space $space --balance
+  fi
 fi
